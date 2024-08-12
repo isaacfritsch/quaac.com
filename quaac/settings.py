691c1ea7183f4bfaac4f38d4375c30cea94e7d6f
isaacfritsch <fritsch.isaac@yahoo.com.br>
2024-08-12 00:34:24 -0300

    problema 500 debug = false

--- quaac/settings.py
+++ quaac/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -132,7 +132,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "templates/static"),
 ]
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
